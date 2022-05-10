a5e666621d1e52bf6d0a2921b7cc8d394e19e79a
Felipe Gonzalez <felipe.gonzalez@foursys.com.br>
2022-05-09 23:58:03 -0300

    Adicionando as funções de calcular Tensão Admissível e Deformação

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,52 @@
-
-
 function CalculaTensaoNormal() {
     var forca = document.querySelector("#forca");
     var diametro = document.querySelector("#diametro");
+    var areaUsuario = document.querySelector("#area");
+
+    console.log(areaUsuario);
 
     var forcaValor = forca.value;
-    var diametroValor = diametro.value; 
-    
-    var raio = diametroValor / 2;
-    var area = Math.PI * (raio * raio);
+    var diametroValor = diametro.value;
+    var areaUsuarioValor = areaUsuario.value;
+    var tensao = 0;
 
-    var tensao = forcaValor / area;
+    console.log(areaUsuarioValor);
+
+    if(areaUsuarioValor > 0) {
+        tensao = forcaValor / areaUsuarioValor;
+
+    } else {
+        var raio = diametroValor / 2;
+        var area = Math.PI * (raio * raio);
+
+        tensao = forcaValor / area;
+    }
 
     document.writeln(tensao);
+}
+
+function CalculaDeformacao() {
+    var comprimentoInicial = document.querySelector("#comprimentoInicial");
+    var variacaoComprimento = document.querySelector("#variacaoComprimento");
+
+    var comprimentoInicialValor = comprimentoInicial.value;
+    var variacaoComprimentoValor = variacaoComprimento.value;
+
+    var deformacao = variacaoComprimentoValor / comprimentoInicialValor;
+
+    document.writeln(deformacao);
+}
+
+function CalculaTensaoAdmissivel() {
+    var tensaoLimite = document.querySelector("#tensaoLimite");
+    var coeficienteSeguranca = document.querySelector("#coeficienteSeguranca");
+
+    var tensaoLimiteValor = tensaoLimite.value;
+    var coeficienteSegurancaValor = coeficienteSeguranca.value;
+
+    var tensaoAdmissivel = tensaoLimiteValor / coeficienteSegurancaValor;
+
+    document.writeln(tensaoAdmissivel);
 }
 
 //var btn = document.getElementById("btnCalcula");
