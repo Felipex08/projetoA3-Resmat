aa7a413d0fc4e5fb677b8126458122e1333f0277
Felipe Gonzalez <felipe.gonzalez@foursys.com.br>
2022-05-13 10:46:46 -0300

    Permitindo o usuário colocar o número com vírgula para fazer os cálculos

--- scripts/script.js
+++ scripts/script.js
@@ -11,9 +11,6 @@ function CalculaTensaoNormal() {
     var forcaValor = ConverteVirgulaEmPonto(forcaValue);
     var diametroValor = ConverteVirgulaEmPonto(diametroValue);
     var areaUsuarioValor = ConverteVirgulaEmPonto(areaUsuarioValue);
-    //console.log(forcaValor);
-    //console.log(diametroValor);
-    //console.log(areaUsuarioValor);
 
     if(areaUsuarioValor > 0) {
         tensao = forcaValor / areaUsuarioValor;
@@ -34,24 +31,34 @@ function CalculaDeformacao() {
     var comprimentoInicial = document.querySelector("#comprimentoInicial");
     var variacaoComprimento = document.querySelector("#variacaoComprimento");
 
-    var comprimentoInicialValor = comprimentoInicial.value;
-    var variacaoComprimentoValor = variacaoComprimento.value;
+    var comprimentoInicialValue = comprimentoInicial.value;
+    var variacaoComprimentoValue = variacaoComprimento.value;
+
+    var comprimentoInicialValor = ConverteVirgulaEmPonto(comprimentoInicialValue);
+    var variacaoComprimentoValor = ConverteVirgulaEmPonto(variacaoComprimentoValue);
 
     var deformacao = variacaoComprimentoValor / comprimentoInicialValor;
 
-    document.querySelector("#respostaDeformacao").innerHTML = "O valor da Deformação é: " + deformacao.toFixed(2) + " m.";
+    var respostaDeformacao = deformacao.toFixed(2).replace(".", ",");
+
+    document.querySelector("#respostaDeformacao").innerHTML = "O valor da Deformação é: " + respostaDeformacao + " m.";
 }
 
 function CalculaTensaoAdmissivel() {
     var tensaoLimite = document.querySelector("#tensaoLimite");
     var coeficienteSeguranca = document.querySelector("#coeficienteSeguranca");
 
-    var tensaoLimiteValor = tensaoLimite.value;
-    var coeficienteSegurancaValor = coeficienteSeguranca.value;
+    var tensaoLimiteValue = tensaoLimite.value;
+    var coeficienteSegurancaValue = coeficienteSeguranca.value;
+
+    var tensaoLimiteValor = ConverteVirgulaEmPonto(tensaoLimiteValue);
+    var coeficienteSegurancaValor = ConverteVirgulaEmPonto(coeficienteSegurancaValue);
 
     var tensaoAdmissivel = tensaoLimiteValor / coeficienteSegurancaValor;
 
-    document.querySelector("#respostaTensaoAdmissivel").innerHTML = "O valor da Tensão Admissível é: " + tensaoAdmissivel.toFixed(2) + " MPa.";
+    var respostaTensaoAdmissivel = tensaoAdmissivel.toFixed(2).replace(".", ",");
+
+    document.querySelector("#respostaTensaoAdmissivel").innerHTML = "O valor da Tensão Admissível é: " + respostaTensaoAdmissivel + " MPa.";
 }
 
 function ConverteVirgulaEmPonto(valor) {
