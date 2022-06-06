3129716f582d852568f3187aaff6a99fc1af8926
Felipe Gonzalez <felipe.gonzalez@foursys.com.br>
2022-06-05 22:39:49 -0300

    Finalmente últimas alterações

--- scripts/deformacao.js
+++ scripts/deformacao.js
@@ -10,11 +10,13 @@ function CalculaDeformacao() {
 
     var deformacao = variacaoComprimentoValor / comprimentoInicialValor;
 
-    var respostaDeformacao = deformacao.toFixed(2).replace(".", ",");
+    var respostaDeformacao = deformacao.toFixed(2);
+
+    var respostaDeformacaoFinal = respostaDeformacao.replace(".", ",");
 
     var respostaDeformacaoPorcentagem = respostaDeformacao * 100;
 
-    document.querySelector("#respostaDeformacao").innerHTML = "Resultado: O valor da Deformação é " + respostaDeformacao + ". Ou em porcentagem é " + respostaDeformacaoPorcentagem + " %.";
+    document.querySelector("#respostaDeformacao").innerHTML = "Resultado: O valor da Deformação é " + respostaDeformacaoFinal + " ou em porcentagem é de " + respostaDeformacaoPorcentagem + "%.";
 }
 
 function ConverteVirgulaEmPonto(valor) {
